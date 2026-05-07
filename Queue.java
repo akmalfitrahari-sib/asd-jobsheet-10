6f05b5d2d3605d7255d6c52fa425b791d6088fa5
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-05-07 22:31:45 +0700

    (Modifikasi supaya program berhenti ketika terjadi queue overflow dan undeflow)

--- Queue.java
+++ Queue.java
@@ -63,6 +63,7 @@ public class Queue {
     public void Enqueue(int dt) {
         if (IsFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         } else {
             if (IsEmpty()) {
                 front = rear = 0;
@@ -82,10 +83,11 @@ public class Queue {
         int dt = 0;
         if (IsEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
-            if (size == 0) {
+            if (IsEmpty()) {
                 front = rear = -1;
             } else {
                 if (front == max - 1) {
